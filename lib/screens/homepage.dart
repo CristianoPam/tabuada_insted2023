b4f99c57c4e66447a3e0c25916c22a999e65ba84
CristianoPam <cpa.cristiano@gmail.com>
2023-10-06 20:07:36 -0400

    sala06102023

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -37,46 +36,47 @@ class _TabuadaScreenState extends State<TabuadaScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          DropdownButton<int>(
-            value: selectedNumber,
-            onChanged: (value) {
-              setState(() {
-                selectedNumber = value!;
-                generateTabuada();
-              });
-            },
-            items: List.generate(
-              10,
-              (index) => DropdownMenuItem<int>(
-                value: index + 1,
-                child: Text((index + 1).toString()),
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            DropdownButton<int>(
+              value: selectedNumber,
+              onChanged: (value) {
+                setState(() {
+                  selectedNumber = value!;
+                  generateTabuada();
+                });
+              },
+              items: List.generate(
+                10,
+                (index) => DropdownMenuItem<int>(
+                  value: index + 1,
+                  child: Text((index + 1).toString()),
+                ),
               ),
             ),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            'Tabuada de $selectedNumber:',
-            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 10),
-          if (tabuada.isNotEmpty)
-            Column(
-              children: tabuada
-                  .map((result) => Text(
-                        '$selectedNumber x ${tabuada.indexOf(result) + 1} = $result',
-                        style: const TextStyle(fontSize: 18),
-                      ))
-                  .toList(),
+            const SizedBox(height: 20),
+            Text(
+              'Tabuada de $selectedNumber:',
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-        ],
+            const SizedBox(height: 10),
+            if (tabuada.isNotEmpty)
+              Column(
+                children: tabuada
+                    .map((result) => Text(
+                          "$selectedNumber x ${(tabuada.indexOf(result) + 1).toString().padLeft(2, '0')} = ${result.toString().padLeft(2, '0')}",
+                          style: const TextStyle(fontSize: 40),
+                        ))
+                    .toList(),
+              ),
+          ],
+        ),
       ),
     );
   }
 }
-
-   
