a3c300af4cdf17c6874e33b9b6c89fd40c59d9ec
CristianoPam <100892314+CristianoPam@users.noreply.github.com>
2023-10-07 14:08:44 -0400

    tema_naruto

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -6,12 +6,23 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner:false,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Tabuada App'),
+        backgroundColor: Colors.yellow,                
+        appBar: AppBar(          
+          centerTitle: true,
+          title: const Text('TABUADA INSTED',style: TextStyle(color: Colors.black)), 
+          backgroundColor: Colors.yellow,                             
         ),
         // ignore: prefer_const_constructors
-        body: TabuadaScreen(),
+        body: Container(
+          margin: null,
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(0),          
+          decoration: const BoxDecoration(
+          image: DecorationImage(image: NetworkImage('https://1.bp.blogspot.com/-tLlFDiULG_w/XykpKtHgOGI/AAAAAAAAL5I/EsPPUEiOKX8SE0BiCI6hub3DcDU1-MthwCLcBGAsYHQ/s2048/Naruto-Uzumaki-wallpaper.png'))  
+          ),
+          child: const TabuadaScreen()),
       ),
     );
   }
@@ -36,46 +47,50 @@ class _TabuadaScreenState extends State<TabuadaScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          // crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            DropdownButton<int>(
-              value: selectedNumber,
-              onChanged: (value) {
-                setState(() {
-                  selectedNumber = value!;
-                  generateTabuada();
-                });
-              },
-              items: List.generate(
-                10,
-                (index) => DropdownMenuItem<int>(
-                  value: index + 1,
-                  child: Text((index + 1).toString()),
-                ),
+    return Padding(              
+      padding: const EdgeInsets.all(17.0),              
+      child: Column(
+        children: <Widget>[
+          DropdownButton<int>(
+            value: selectedNumber,
+            alignment: Alignment.bottomCenter,
+            elevation: 16,              
+            style: const TextStyle(color: Colors.black,fontSize: 30, fontWeight: FontWeight.w800),
+            underline: Container(height: 2, color: Color.fromARGB(255, 243, 219, 3),),
+            onChanged: (value) {
+              setState(() {
+                selectedNumber = value!;
+                generateTabuada();
+              });
+            },
+            items: List.generate(
+              10,
+              (index) => DropdownMenuItem<int>(
+                value: index + 1,
+                child: Text((index + 1).toString()),
               ),
             ),
-            const SizedBox(height: 20),
-            Text(
-              'Tabuada de $selectedNumber:',
-              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 1),
+          Text(
+            'Tabuada de $selectedNumber:',
+            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 10),
+          if (tabuada.isNotEmpty)
+            Column(
+
+              children: tabuada
+                  .map((result) => Container(
+                    color: Colors.black26,
+                    child: Text(
+                          "$selectedNumber  x  ${(tabuada.indexOf(result) + 1).toString().padLeft(2, '0')}  =  ${result.toString().padLeft(2, '0')}",
+                          style: const TextStyle(fontSize: 45, color: Color.fromARGB(255, 243, 219, 3),fontWeight: FontWeight.w700),
+                        ),
+                  ))
+                  .toList(),
             ),
-            const SizedBox(height: 10),
-            if (tabuada.isNotEmpty)
-              Column(
-                children: tabuada
-                    .map((result) => Text(
-                          "$selectedNumber x ${(tabuada.indexOf(result) + 1).toString().padLeft(2, '0')} = ${result.toString().padLeft(2, '0')}",
-                          style: const TextStyle(fontSize: 40),
-                        ))
-                    .toList(),
-              ),
-          ],
-        ),
+        ],
       ),
     );
   }
